a0425131436eac89d5403e48c1de9f290e924e71
Conrad Wild <cwild@localhost.localdomain>
2019-01-08 13:33:20 +0100

    changed functionality for RTCCounter class

--- lib/RTC/src/RTCCounter.cpp
+++ lib/RTC/src/RTCCounter.cpp
@@ -1,11 +1,11 @@
 /*
- * EventCounter.cpp
+ * RTCCounter.cpp
  *
  *  Created on: 02.01.2019
  *      Author: cwild
  */
 
-#include <EventCounter.h>
+#include <RTCCounter.h>
 
 #include "Arduino.h"
 extern "C" {
@@ -14,33 +14,47 @@ extern "C" {
 
 namespace rtc {
 
-EventCounter::EventCounter() {}
+RTCCounter::RTCCounter(uint8_t mem_loc) : mem_location(mem_loc) {}
 
-EventCounter::~EventCounter() {}
+RTCCounter::~RTCCounter() {}
 
-void EventCounter::init() {
-	rtc_evcnt_t e;
-	e.evcnt = 0;
-	e.evcnt_mirror = 0xffffffff;
-	system_rtc_mem_write(sysdefs::rtc::rtc_eventcnt, &e, sizeof(e));
+void RTCCounter::init() {
+	rtc_cnt_t e;
+	e.cnt = 0;
+	e.cnt_mirror = 0xffffffff;
+	system_rtc_mem_write(mem_location, &e, sizeof(e));
 }
 
-int EventCounter::get_evcnt() {
-	rtc_evcnt_t e;
+int RTCCounter::get_cnt() {
+	rtc_cnt_t e;
 	int rv;
-	system_rtc_mem_read(sysdefs::rtc::rtc_eventcnt, &e, sizeof(e));
-	if ((e.evcnt ^ e.evcnt_mirror) == 0xffffffff)
-		rv = e.evcnt;
+	system_rtc_mem_read(mem_location, &e, sizeof(e));
+	if ((e.cnt ^ e.cnt_mirror) == -1)
+		rv = e.cnt;
 	else
 		rv = -1;
 	return rv;
 }
 
-void EventCounter::set_evcnt(int val) {
-	rtc_evcnt_t e;
-	e.evcnt = val;
-	e.evcnt_mirror = ~val;
-	system_rtc_mem_write(sysdefs::rtc::rtc_eventcnt, &e, sizeof(e));
+void RTCCounter::set_cnt(int val) {
+	rtc_cnt_t e;
+	e.cnt = val;
+	e.cnt_mirror = ~val;
+	system_rtc_mem_write(mem_location, &e, sizeof(e));
+}
+
+void RTCCounter::inc() {
+	int c_val;
+	c_val = this->get_cnt();
+	++c_val;
+	this->set_cnt(c_val);
+}
+
+void RTCCounter::dec() {
+	int c_val;
+	c_val = this->get_cnt();
+	--c_val;
+	this->set_cnt(c_val);
 }
 
 } /* namespace rtc */
--- lib/RTC/src/RTCCounter.h
+++ lib/RTC/src/RTCCounter.h
@@ -3,30 +3,37 @@
  *
  *  Created on: 02.01.2019
  *      Author: cwild
+ *
+ *      This is the baseclass for a counter that persists in RTC ram. Needs 8 Byte per counter.
  */
 
-#ifndef LIB_RTC_SRC_EVENTCOUNTER_H_
-#define LIB_RTC_SRC_EVENTCOUNTER_H_
+#ifndef LIB_RTC_SRC_RTCCOUNTER_H_
+#define LIB_RTC_SRC_RTCCOUNTER_H_
 
 #include <system_definitions.h>
 
 namespace rtc {
 
 typedef struct {
-	int evcnt;
-	int evcnt_mirror;
-} rtc_evcnt_t;
+	int cnt;
+	int cnt_mirror;
+} rtc_cnt_t;
 
-class EventCounter {
+class RTCCounter {
 public:
-	EventCounter();
-	virtual ~EventCounter();
+	RTCCounter(uint8_t mem_loc);
+	virtual ~RTCCounter();
 
 	void init();
-	int get_evcnt();
-	void set_evcnt(int val);
+	virtual int get_cnt();
+	virtual void set_cnt(int val);
+	virtual void inc();
+	virtual void dec();
+
+private:
+	uint8_t mem_location;
 };
 
 } /* namespace rtc */
 
-#endif /* LIB_RTC_SRC_EVENTCOUNTER_H_ */
+#endif /* LIB_RTC_SRC_RTCCOUNTER_H_ */
