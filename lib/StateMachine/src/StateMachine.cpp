342f0914d0bc152e5baf15272521131ef7d49bea
Conrad Wild <cwild@local.host>
2019-12-02 13:29:28 +0100

    changes due to compiler warnings

--- lib/StateMachine/src/StateMachine.cpp
+++ lib/StateMachine/src/StateMachine.cpp
@@ -9,13 +9,13 @@
 #include "StateMachine.h"
 
 StateMachine::StateMachine(int maxStates) :
-		_maxStates(maxStates), currentState(0), _eventGenerated(false), _pEventData(
-		NULL),cycleTime(0) {
+		currentState(0), _maxStates(maxStates), _eventGenerated(false), _pEventData(
+		NULL), cycleTime(0) {
 }
 
 // generates an external event. called once per external event
 // to start the state machine executing
-void StateMachine::ExternalEvent(unsigned char newState, EventData* pData) {
+void StateMachine::ExternalEvent(unsigned char newState, EventData *pData) {
 	// if we are supposed to ignore this event
 	if (newState == EVENT_IGNORED) {
 		// just delete the event data, if any
@@ -30,7 +30,7 @@ void StateMachine::ExternalEvent(unsigned char newState, EventData* pData) {
 
 // generates an internal event. called from within a state
 // function to transition to a new state
-void StateMachine::InternalEvent(unsigned char newState, EventData* pData) {
+void StateMachine::InternalEvent(unsigned char newState, EventData *pData) {
 	_pEventData = pData;
 	_eventGenerated = true;
 	currentState = newState;
@@ -48,7 +48,7 @@ unsigned long StateMachine::getCycleTime() {
 
 // the state engine executes the state machine states
 void StateMachine::StateEngine(void) {
-	EventData* pDataTemp = NULL;
+	EventData *pDataTemp = NULL;
 
 	// TBD - lock semaphore here
 	// while events are being generated keep executing states
@@ -60,7 +60,7 @@ void StateMachine::StateEngine(void) {
 		assert(currentState < _maxStates);
 
 		// execute the state passing in event data, if any
-		const StateStruct* pStateMap = GetStateMap();
+		const StateStruct *pStateMap = GetStateMap();
 		(this->*pStateMap[currentState].pStateFunc)(pDataTemp);
 
 		// if event data was used, then delete it
